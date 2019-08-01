5535a4ce55aeddfb8c59897ee8f536fe94406c97
florex20 <38489584+florex20@users.noreply.github.com>
2019-08-01 18:34:24 +0800

    create

---
+++ assignment.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std ;
+
+class Passenger
+{
+public:
+	string p_name , passport_No ;
+	int m_card ;
+
+	void creditMile() ;
+	void consumeMiles();
+	void cancelMiles();
+	
+};
+
+class MilesAccount
+{
+public:
+	int p_accnum ; f_miles;
+	
+};
+
+class Booking
+{
+public:
+	int date ;
+	char tier ;
+
+	void reserve();
+	void pay();
+	void cancel();
+	void change();
+	
+};
+
+class Flight
+{
+public:
+	int time , date, miles ;
+};
