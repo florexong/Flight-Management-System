2d6a12a9df39e77848eef8404a3329c861238417
rishidharan99 <39621712+rishidharan99@users.noreply.github.com>
2019-08-06 00:25:09 +0800

    Update assignment.cpp
    problem check for empty file and same element already solve

--- assignment.cpp
+++ assignment.cpp
@@ -18,13 +18,26 @@ private:
 	int passno;
 	string Fname;
 	string Lname;
+	string Title;
 	vector<int> passportnum;
+	vector<string> DataFN;
+	vector<string> DataLN;
+	vector<string> DataTT;
 public:
 	int p_num;
-	void set_passangerinfo();
+
+
+	void set_passangerinfo();//pasport
 	void set_passno(){ passno = p_num;}
 	int get_passno(){return passno;}
+	bool check_element();
+	void set_allinfo();//all info of passanger
+	string get_Fname(){ return Fname;}
+	string get_Lname(){ return Lname;}
+	string get_Title(){ return Title;}
 	 vector<int>::iterator i;
+
+
 };
 
 
@@ -34,11 +47,39 @@ private:
 public:
 	void display_main();
 
+
 };
 
 
-//################# Passanger ############################
+//########### Passanger #######################
+
+
+void Passanger::set_allinfo(){
+	this->Fname = Fname;
+	this->Lname = Lname;
+	this->Title = Title;
+}
+
+
+bool Passanger::check_element()
+{
+	bool a;
+	for(i= passportnum.begin();i != passportnum.end();i++)
+	{
+		if(*i == get_passno())
+		{
+			a =  false;
+		}
+		else{
+			a=  true;
+
+		}
+	}
+	return a;
+
+}
 void Passanger::set_passangerinfo(){
+    string over;
 	cout<< " Welcome to Create Account\n";
 	cout << "Enter Passport Number : ";
 	cin >> p_num;
@@ -46,25 +87,102 @@ void Passanger::set_passangerinfo(){
 	get_passno();
 	if(!passportnum.empty())
 	{
-		for(i=passportnum.begin(); i != passportnum.end();++i)
+		if(check_element() == true)
 		{
-			if(*i == get_passno()){
-				cout<<"The User is Exists ! \n";
-			}
-			else{
-				cout<<"this new";
-				numpas++;
-				passportnum.push_back(get_passno());
-				cout<<numpas;
-				break;
-			}
+			passportnum.push_back(get_passno());
+			getline(cin,over);
+			cout << "Enter First Name : ";
+			getline(cin, Fname);
+			cout << endl;
+			cout << "Enter Last Name : ";
+			getline(cin, Lname);
+			cout << endl;
+			cout << "Enter Title  : ";
+			getline(cin, Title);
+			cout << endl;
+
+			DataFN.push_back(get_Fname());
+			DataLN.push_back(get_Lname());
+			DataTT.push_back(get_Title());
+
+
 		}
+		else{
+			cout<<"this Account Already Registered \n";
+
+		}
+
 	}
 	else
 	{
 		passportnum.push_back(get_passno());
+		getline(cin,over);
+		cout << "Enter First Name : ";
+		getline(cin, Fname);
+		cout << endl;
+		cout << "Enter Last Name : ";
+		getline(cin, Lname);
+		cout << endl;
+		cout << "Enter Title  : ";
+		getline(cin, Title);
+		cout << endl;
+
+		DataFN.push_back(get_Fname());
+		DataLN.push_back(get_Lname());
+		DataTT.push_back(get_Title());
 
 	}
+	// for(int i = 0;i < passportnum.size();i++)
+	// {
+	// 	cout<<passportnum[i]<<DataFN[i]<<DataLN[i]<<DataTT[i]<<"\n";
+	// }
 	
+
+}
+
+
+
+
+
+
+
+
+
+//################## Main_System ###########################
+void Main_System::display_main(){
+	bool flag = true;
+	int choice;
+
+	while(flag)
+	{
+		cout<< "Welcome to Airline \n";
+		cout<< "1. Create Account \n";
+		cout<< "2. Exists Account \n";
+		cin >> choice;
+		if(choice == 1)
+		{
+			set_passangerinfo();
+
+		}
+
+
+	}
+
+
+}
+
+
+
+int main()
+{
+	Main_System main;
+	main.display_main();
+
+
+
+
+
+ return 0 ;
+
 }
 
